cce546439f7c6da86c8dc9753927c867e9e1e6fc
Dmitry Sharabin <dmitrysharabin@gmail.com>
2026-01-20 18:12:56 +0100

    Add simple test

---
+++ tests/headless.js
@@ -0,0 +1,21 @@
+export default {
+	name: "Headless runner tests",
+	tests: [
+		{
+			name: "Simple math",
+			args: [1, 41],
+			run: (a, b) => a + b,
+			expect: 42,
+		},
+		{
+			name: "DOM APIs",
+			run: () => {
+				let div = document.createElement("div");
+				div.textContent = "Headless runner works!";
+				document.body.append(div);
+				return getComputedStyle(div).display;
+			},
+			expect: "block",
+		},
+	],
+};
